58118d4b1b47b85899c958bbf7afcaa3a886c207
Sardor Sobirov <sardorbsobirov@gmail.com>
2025-12-06 00:12:11 -0800

    fixed path

--- sardor_sobirov/assignment3/Code/Vizualization Code/PA3-Viz4.sql
+++ sardor_sobirov/assignment3/Code/Vizualization Code/PA3-Viz4.sql
@@ -1,17 +1,24 @@
--- 1. Structure count by land cover type (bar chart)
-select 
-  land_cover_subtype,
-  sum(structure_count) as total_structures
-from `capstone2025.CA_PropertyStats_ByLandCover`
-group by land_cover_subtype
-order by total_structures desc;
+with county_stats as (
+  select 
+    fips,
+    land_cover_subtype,
+    sum(total_structures) as total_structures,
+    sum(mean_content_cost * total_structures) / sum(total_structures) 
+      as weighted_mean_content_cost
+  from `capstone2025.CA_PropertyStats_Summary`
+  where land_cover_subtype in ('urban', 'forest', 'shrub')
+  group by fips, land_cover_subtype
+)
 
--- 2. Building types by land cover (stacked bar)
-select 
-  land_cover_subtype,
-  bldgtype,
-  sum(structure_count) as structure_count
-from `capstone2025.CA_PropertyStats_ByLandCover`
-group by land_cover_subtype, bldgtype
-order by land_cover_subtype, structure_count desc;
+select
+  c.county_name,
+  cs.fips,
+  cs.land_cover_subtype,
+  cs.weighted_mean_content_cost as mean_content_cost,
+  c.county_geom as geometry
+from county_stats cs
+join `bigquery-public-data.geo_us_boundaries.counties` c
+  on cs.fips = c.county_fips_code
+where c.state_fips_code = '06';
+
 
--- sardor_sobirov/assignment3/Code/Vizualization Code/PA3-Viz5.sql
+++ sardor_sobirov/assignment3/Code/Vizualization Code/PA3-Viz5.sql
@@ -1,6 +1,15 @@
-select 
-  land_cover_subtype,
-  sum(structure_count) as total_structures
-from `capstone2025.CA_PropertyStats_ByLandCover`
-group by land_cover_subtype
-order by total_structures desc;
+with county_stats as (
+  select 
+    fips,
+    sum(total_structures) as total_structures,
+    sum(mean_content_cost * total_structures) / sum(total_structures) 
+      as mean_content_cost
+  from `capstone2025.CA_PropertyStats_Summary`
+  where land_cover_subtype = 'urban'
+  group by fips
+)
+
+select
+  min(mean_content_cost) as min_mean_content_cost,
+  max(mean_content_cost) as max_mean_content_cost
+from county_stats;
